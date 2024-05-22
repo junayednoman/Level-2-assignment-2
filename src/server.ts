4f4bbfbaf8bc785b20f5f404651bfc9313fd186f
junayednoman <junayednoman05@gmail.com>
2024-05-22 17:11:41 +0600

    initial setup done

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import config from './app/config';
+import app from './app';
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`App listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
